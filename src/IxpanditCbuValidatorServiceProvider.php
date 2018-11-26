1e0466d9c31fbda169e8a9c54f43eda33fa8c820
Greycivier Espinoza <gespinoza@Greyciviers-MacBook-Air.local>
2018-11-26 18:16:57 -0300

    changes owner validation for products

--- src/IxpanditCbuValidatorServiceProvider.php
+++ src/IxpanditCbuValidatorServiceProvider.php
@@ -20,51 +20,45 @@ class IxpanditCbuValidatorServiceProvider extends ServiceProvider
 			$data = $validator->getData();
 			$cbuValidatorService = new CbuValidatorService(new CbuValidatorAdapter());
 			$validate = $cbuValidatorService->validate($data['cbu'], $data['uniqueIdentifier']);
-			
+
 			if(isset($data['business']) && $data['business'] != "grouit" && $validate['validated'] && $validate['type'] == "CC")
 			{
 				$validator->setFallbackMessages(['ixpandit_cbu_hard' => "El CBU ingresado corresponde a una Cuenta Corriente."]);
 				return false;
 			}
-			
+
 			if(isset($validate['owners']) && $validate['owners'] > 1)
 			{
 				$validator->setFallbackMessages(['ixpandit_cbu_hard' => "El CBU ingresado tiene más de un titular."]);
 				return false;
 			}
-			
+
 			if(!$validate['validated'])
 			{
 				$validator->setFallbackMessages(['ixpandit_cbu_hard' => $validate['message']]);
 				return false;
 			}
-			
+
 			return $validate['validated'];
 		});
-		
+
 		$this->app['validator']->extend('ixpandit_cbu_soft', function ($attribute, $value, $parameter, $validator)
 		{
 			$data = $validator->getData();
 			$cbuValidatorService = new CbuValidatorService(new CbuValidatorAdapter());
 			$validate = $cbuValidatorService->validate($data['cbu'], $data['uniqueIdentifier']);
-			
+
 			if(isset($data['business']) && $data['business'] != "grouit" && $validate['validated'] && $validate['type'] == "CC")
 			{
 				$validator->setFallbackMessages(['ixpandit_cbu_soft' => "El CBU ingresado corresponde a una Cuenta Corriente."]);
 				return false;
 			}
 			
-			if(isset($validate['owners']) && $validate['owners'] > 1)
-			{
-				$validator->setFallbackMessages(['ixpandit_cbu_soft' => "El CBU ingresado tiene más de un titular."]);
-				return false;
-			}
-
 			if(!$validate['validated'] && isset($validate['error']) && $validate['error'] == "API_BANK_ERROR")
 			{
 				return true;
 			}
-			
+
 			if(!$validate['validated'])
 			{
 				$validator->setFallbackMessages(['ixpandit_cbu_soft' => $validate['message']]);
@@ -73,7 +67,7 @@ class IxpanditCbuValidatorServiceProvider extends ServiceProvider
 
 			return $validate['validated'];
 		});
-		
+
 		$this->app['validator']->replacer('ixpandit_cbu_hard', function ($message, $attribute, $rule, $parameters) {
 			return $message;
 		});
